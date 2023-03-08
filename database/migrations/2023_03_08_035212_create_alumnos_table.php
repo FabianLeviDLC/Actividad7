e770f72e9901d2728a22923cc0374e2109d1e59b
FabianLeviDLC <AL02979388@TECMILENIO.MX>
2023-03-07 22:24:56 -0600

    Iniciamos los cambios

---
+++ database/migrations/2023_03_08_035212_create_alumnos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id('Student_Id');
+            $table->string('FName');
+            $table->string('LName');
+            $table->Integer('Group_Id');
+            $table->Integer('Course_Id');
+            $table->Integer('User_Id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
